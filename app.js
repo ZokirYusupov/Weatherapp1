7c4bc3cf42e286059f9952b4cd0cc454db8c6130
Zokirbek <zyusupov51@mail.com>
2022-01-12 14:16:34 +0500

    s

--- app.js
+++ app.js
@@ -1,62 +1,79 @@
+
+
 const api = {
-    key: '4ab23a2af5944421d57379da8a3da38e',
+    key: '8f2100535c0e1250b2ddc4bc7fdc29c9',
     baseurl:'https://api.openweathermap.org/data/2.5/'
 };
+let query = 'Usa'
+const request = new XMLHttpRequest();
 
-const searchBox = document.querySelector('.search-box');
-searchBox.addEventListener('keypress',setQuery);
+request.open('GET',`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+request.responseType = 'json'
 
-
-function setQuery(e){
-    if(e.keyCode === 13){
-        console.log(searchBox.value);
-        getResualts(searchBox.value);
-        
+request.addEventListener('load', () => {
+    if(request.status === 200){
+        console.log(request.response);
     }
-}
-function getResualts(query){
-    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then(
-        weather => {
-            return weather.json();
-        }
-    )
-    .then(displayResualts);
+})
+request.send()
+// const searchBox = document.querySelector('.search-box');
+// searchBox.addEventListener('keypress',setQuery);
 
-}
-function displayResualts(weather){
-    console.log(weather);
-    let city = document.querySelector('.location .city')
-    city.innerHTML = `
-    ${weather.name}, ${weather.sys.country}
-    `
-    let now = new Date();
-    let date = document.querySelector('.date');
-    date.innerHTML = dateBuilder(now);
 
-    let temp = document.querySelector('.temp');
+// function setQuery(e){
+//     if(e.keyCode === 13){
+//         // console.log(searchBox.value);
+//         getResualts(searchBox.value);
+        
+//     }
+// }
+// function getResualts(query){
+    // fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    // APPID ni kichik harfada yozsak ham boladi
+//     .then(
+//         weather => {
+//             return weather.json();
+//         }
+//     )
+//     .then(displayResualts);
 
-    temp.innerHTML = `
-    ${Math.floor(weather.main.temp)}<span>°C</span>
-    `
-    let weatherEl = document.querySelector('.weather');
+// }
+// function displayResualts(weather){
+//     console.log(weather);
+//     let city = document.querySelector('.location .city')
+//     city.innerHTML = `
+//     ${weather.name}, ${weather.sys.country}
+//     `
+//   document.getElementById("weatherIconData").src = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+    
 
-    weatherEl.innerHTML = weather.weather[0].main
+//     let now = new Date();
+//     let date = document.querySelector('.date');
+//     date.innerHTML = dateBuilder(now);
 
-    let hilow = document.querySelector('.hi-low');
+//     let temp = document.querySelector('.temp');
 
-    hilow.innerHTML = `${Math.floor(weather.main.temp_min)}°C/${Math.floor(weather.main.temp_max)}°C`
-}
+//     temp.innerHTML = `
+//     ${Math.floor(weather.main.temp)}<span>°C</span>
+//     `
+//     let weatherEl = document.querySelector('.weather');
 
-function dateBuilder(s){
-    let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+//     weatherEl.innerHTML = weather.weather[0].main
 
-    let day = days[s.getDay()]
-    let date = s.getDate()
-    let month = months[s.getMonth()];
+//     let hilow = document.querySelector('.hi-low');
 
-    let year = s.getFullYear();
-    return `${day} ${date} ${month} ${year}`
+//     hilow.innerHTML = `${Math.floor(weather.main.temp_min)}°C/${Math.floor(weather.main.temp_max)}°C`
+// }
 
-}
+// function dateBuilder(s){
+//     let months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+//     let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+//     let day = days[s.getDay()]
+//     let date = s.getDate()
+//     let month = months[s.getMonth()];
+
+//     let year = s.getFullYear();
+//     return `${day} ${date} ${month} ${year}`
+
+// }
